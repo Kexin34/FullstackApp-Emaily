c199d2ab776193c34fbd0796e46f2d79d4e28b72
Kexin34 <kexinwen.ca@gmail.com>
2020-10-30 17:27:47 -0700

    Added billing and client side app

--- index.js
+++ index.js
@@ -24,5 +24,21 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets like our main.js file, or main.css file!
+  // 让它look into client/build目录来找
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file if it doesn't recognize the route
+  // assume the react-side of appliction will recognaize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+}
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
